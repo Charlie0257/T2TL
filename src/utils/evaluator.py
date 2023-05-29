25fd2476b80c839e4c386516a9088487fa5e7224
Charlie0257 <53024517+Charlie0257@users.noreply.github.com>
2023-05-29 16:31:52 +0800

    add utils module

---
+++ src/utils/evaluator.py
@@ -0,0 +1,147 @@
+import time
+import torch
+from torch_ac.utils.penv import ParallelEnv
+#import tensorboardX
+
+import utils
+import argparse
+import datetime
+
+class Eval:
+    def __init__(self, env, model_name, ltl_sampler,
+                seed=0, device="cpu", argmax=False,
+                num_procs=1, ignoreLTL=False, progression_mode=True, gnn=None, recurrence=1, dumb_ac = False, discount=0.99):
+
+        self.env = env
+        self.device = device
+        self.argmax = argmax
+        self.num_procs = num_procs
+        self.ignoreLTL = ignoreLTL
+        self.progression_mode = progression_mode
+        self.gnn = gnn
+        self.recurrence = recurrence
+        self.dumb_ac = dumb_ac
+        self.discount = discount
+
+        self.model_dir = utils.get_model_dir(model_name, storage_dir="")
+        #self.tb_writer = tensorboardX.SummaryWriter(self.model_dir + "/eval-" + ltl_sampler)
+
+        # Load environments for evaluation
+        eval_envs = []
+        for i in range(self.num_procs):
+            eval_envs.append(utils.make_env(env, progression_mode, ltl_sampler, seed, 0, False))
+
+        eval_envs[0].reset()
+        if isinstance(eval_envs[0].env, LetterEnv):
+            for env in eval_envs:
+                env.env.map = eval_envs[0].env.map
+
+        self.eval_envs = ParallelEnv(eval_envs)
+
+
+
+
+    def eval(self, num_frames, episodes=100, stdout=True):
+        # Load agent
+            
+        agent = utils.Agent(self.eval_envs.envs[0], self.eval_envs.observation_space, self.eval_envs.action_space, self.model_dir + "/train", 
+            self.ignoreLTL, self.progression_mode, self.gnn, recurrence = self.recurrence, dumb_ac = self.dumb_ac, device=self.device, argmax=self.argmax, num_envs=self.num_procs)
+
+
+        # Run agent
+        start_time = time.time()
+
+        obss = self.eval_envs.reset()
+        log_counter = 0
+
+        log_episode_return = torch.zeros(self.num_procs, device=self.device)
+        log_episode_num_frames = torch.zeros(self.num_procs, device=self.device)
+
+        # Initialize logs
+        logs = {"num_frames_per_episode": [], "return_per_episode": []}
+        while log_counter < episodes:
+            actions = agent.get_actions(obss)
+            obss, rewards, dones, _ = self.eval_envs.step(actions)
+            agent.analyze_feedbacks(rewards, dones)
+
+            log_episode_return += torch.tensor(rewards, device=self.device, dtype=torch.float)
+            log_episode_num_frames += torch.ones(self.num_procs, device=self.device)
+
+            for i, done in enumerate(dones):
+                if done:
+                    log_counter += 1
+                    logs["return_per_episode"].append(log_episode_return[i].item())
+                    logs["num_frames_per_episode"].append(log_episode_num_frames[i].item())
+
+            mask = 1 - torch.tensor(dones, device=self.device, dtype=torch.float)
+            log_episode_return *= mask
+            log_episode_num_frames *= mask
+
+        end_time = time.time()
+
+
+        return logs["return_per_episode"], logs["num_frames_per_episode"]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ltl-sampler", default="Default",
+                    help="the ltl formula template to sample from (default: DefaultSampler)")
+    parser.add_argument("--seed", type=int, default=1,
+                    help="random seed (default: 1)")
+    parser.add_argument("--model-paths", required=True, nargs="+",
+                    help="path of the model, or a regular expression")
+    parser.add_argument("--procs", type=int, default=1,
+                    help="number of processes (default: 1)")
+    parser.add_argument("--eval-episodes", type=int,  default=5,
+                    help="number of episodes to evaluate on (default: 5)")
+    parser.add_argument("--env", default="Letter-7x7-v3",
+                        help="name of the environment to train on (REQUIRED)")
+    parser.add_argument("--discount", type=float, default=0.99,
+                    help="discount factor (default: 0.99)")
+
+    parser.add_argument("--ignoreLTL", action="store_true", default=False,
+                    help="the network ignores the LTL input")
+    parser.add_argument("--progression-mode", default="full",
+                    help="Full: uses LTL progression; partial: shows the propositions which progress or falsify the formula; none: only original formula is seen. ")
+    parser.add_argument("--recurrence", type=int, default=1,
+                    help="number of time-steps gradient is backpropagated (default: 1). If > 1, a LSTM is added to the model to have memory.")
+    parser.add_argument("--gnn", default="RGCN_8x32_ROOT_SHARED", help="use gnn to model the LTL (only if ignoreLTL==True)")
+
+
+    args = parser.parse_args()
+
+    logs_returns_per_episode = []
+    logs_num_frames_per_episode = [] 
+
+    for model_path in args.model_paths:
+        idx = model_path.find("seed:") + 5
+        seed = int(model_path[idx:idx+2].strip("_"))
+
+        eval = utils.Eval(args.env, model_path, args.ltl_sampler,
+                     seed=seed, device=torch.device("cpu"), argmax=False,
+                     num_procs=args.procs, ignoreLTL=args.ignoreLTL, progression_mode=args.progression_mode, gnn=args.gnn, recurrence=args.recurrence, dumb_ac=False, discount=args.discount)
+        rpe, nfpe = eval.eval(-1, episodes=args.eval_episodes, stdout=True)
+        logs_returns_per_episode += rpe
+        logs_num_frames_per_episode += nfpe 
+
+        print(sum(rpe), seed, model_path)
+
+    print(logs_num_frames_per_episode)
+    print(logs_returns_per_episode)
+    num_frame_pe = sum(logs_num_frames_per_episode)
+    return_per_episode = utils.synthesize(logs_returns_per_episode)
+    num_frames_per_episode = utils.synthesize(logs_num_frames_per_episode)
+    average_discounted_return, error = utils.average_discounted_return(logs_returns_per_episode, logs_num_frames_per_episode, args.discount, include_error=True)
+
+    header = ["frames"]
+    data   = [num_frame_pe]
+    header += ["num_frames_" + key for key in num_frames_per_episode.keys()]
+    data += num_frames_per_episode.values()
+    header += ["average_discounted_return", "err"]
+    data += [average_discounted_return, error]
+
+    header += ["return_" + key for key in return_per_episode.keys()]
+    data += return_per_episode.values()
+
+    for field, value in zip(header, data):
+        print(field, value)
---
+++ src/utils/format.py
@@ -0,0 +1,127 @@
+"""
+These functions preprocess the observations.
+When trying more sophisticated encoding for LTL, we might have to modify this code.
+"""
+
+import os
+import json
+import re
+import torch
+import torch_ac
+import gym
+import numpy as np
+import utils
+
+from envs import *
+from ltl_wrappers import LTLEnv
+
+def get_obss_preprocessor(env, gnn, progression_mode, gnn_type=None):
+    obs_space = env.observation_space
+    vocab_space = env.get_propositions()
+    vocab = None
+
+    if isinstance(env, LTLEnv):  # LTLEnv Wrapped env
+        env = env.unwrapped
+        if isinstance(env, ZonesEnv):
+            if progression_mode == "partial":
+                obs_space = {"image": obs_space.spaces["features"].shape, "progress_info": len(vocab_space)}
+                def preprocess_obss(obss, device=None):
+                    return torch_ac.DictList({
+                        "image": preprocess_images([obs["features"] for obs in obss], device=device),
+                        "progress_info":  torch.stack([torch.tensor(obs["progress_info"], dtype=torch.float) for obs in obss], dim=0).to(device)
+                    })
+
+            else:
+                obs_space = {"image": obs_space.spaces["features"].shape, "text": max(22, len(vocab_space) + 10)}
+                vocab_space = {"max_size": obs_space["text"], "tokens": vocab_space}
+
+                vocab = Vocabulary(vocab_space)
+                tree_builder = utils.ASTBuilder(vocab_space["tokens"])
+                def preprocess_obss(obss, device=None):
+                    return torch_ac.DictList({
+                        "image": preprocess_images([obs["features"] for obs in obss], device=device),
+                        "text":  preprocess_texts([obs["text"] for obs in obss], vocab, vocab_space, gnn=gnn, gnn_type=gnn_type, device=device, ast=tree_builder)
+                    })
+
+            preprocess_obss.vocab = vocab
+                                           
+
+        else:
+            raise ValueError("Unknown observation space: " + str(obs_space))
+    # Check if obs_space is an image space
+    elif isinstance(obs_space, gym.spaces.Box):
+        obs_space = {"image": obs_space.shape}
+
+        def preprocess_obss(obss, device=None):
+            return torch_ac.DictList({
+                "image": preprocess_images(obss, device=device)
+            })
+    else:
+        raise ValueError("Unknown observation space: " + str(obs_space))
+
+    return obs_space, preprocess_obss
+
+
+def preprocess_images(images, device=None):
+    # Bug of Pytorch: very slow if not first converted to numpy array
+    images = np.array(images)  # list:16 (each list : len(list)=76) -> {ndarray: (16, 76)}
+    return torch.tensor(images, device=device, dtype=torch.float)
+
+
+def preprocess_texts(texts, vocab, vocab_space, gnn=False, gnn_type=None, device=None, **kwargs):
+    
+    if (gnn):
+        return preprocess4gnn(texts, kwargs["ast"], device)
+
+    return preprocess4rnn(texts, vocab, device)
+
+
+def preprocess4rnn(texts, vocab, device=None):
+
+    var_indexed_texts = []
+    max_text_len = 25
+
+    for text in texts:
+        text = str(text) # transforming the ltl formula into a string
+        tokens = re.findall("([a-z]+)", text.lower())
+        var_indexed_text = np.array([vocab[token] for token in tokens])
+        var_indexed_texts.append(var_indexed_text)
+        max_text_len = max(len(var_indexed_text), max_text_len)
+
+    indexed_texts = np.zeros((len(texts), max_text_len))
+
+    for i, indexed_text in enumerate(var_indexed_texts):
+        indexed_texts[i, :len(indexed_text)] = indexed_text
+
+    return torch.tensor(indexed_texts, device=device, dtype=torch.long)
+
+def preprocess4gnn(texts, ast, device=None):
+    """
+    This function receives the LTL formulas and convert them into inputs for a GNN
+    """
+    return np.array([[ast(text).to(device)] for text in texts])
+
+class Vocabulary:
+    """A mapping from tokens to ids with a capacity of `max_size` words.
+    It can be saved in a `vocab.json` file."""
+
+    def __init__(self, vocab_space):
+        self.max_size = vocab_space["max_size"]
+        self.vocab = {}
+
+        # populate the vocab with the LTL operators
+        for item in ['next', 'until', 'and', 'or', 'eventually', 'always', 'not', 'True', 'False']:
+            self.__getitem__(item)
+
+        for item in vocab_space["tokens"]:
+            self.__getitem__(item)
+
+    def load_vocab(self, vocab):
+        self.vocab = vocab
+
+    def __getitem__(self, token):
+        if not token in self.vocab.keys():
+            if len(self.vocab) >= self.max_size:
+                raise ValueError("Maximum vocabulary capacity reached")
+            self.vocab[token] = len(self.vocab) + 1
+        return self.vocab[token]
